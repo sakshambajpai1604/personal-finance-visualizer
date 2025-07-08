5f24d2cda261c80db1f338be65313e4025dd0b11
sakshambajpai1604 <sakshambajpai.1604@gmail.com>
2025-07-08 20:12:56 +0530

    MongoDB Connection and Transaction Model

---
+++ models/Transaction.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const TransactionSchema = new mongoose.Schema({
+  amount: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: true });
+
+export default mongoose.models.Transaction || mongoose.model('Transaction', TransactionSchema);
